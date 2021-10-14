5442b407c6a84021d6fbda5193be36cc38a8f06c
Yasin BİRCAN <yasinbrcn@gmail.com>
2021-10-14 23:35:39 +0300

    [+]: Second exercise of code has been added.

---
+++ Exercises_02/method1_examples.py
@@ -0,0 +1,24 @@
+"""
+Created on Thu Oct 14 21:41:07 2021
+
+@author: Yasin BİRCAN
+"""
+
+
+#Example_1:
+'''
+import numpy as np
+print(np.zeros(10))
+'''
+
+#Example_2:
+'''
+import numpy as np
+print(np.zeros(10,dtype="int"))
+'''
+
+#Example_3:
+'''
+import numpy as np
+print(np.zeros((3,5),dtype="int"))
+'''
